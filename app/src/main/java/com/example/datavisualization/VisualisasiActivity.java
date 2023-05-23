8e1ba3fa6538cc389610f1479883cd6b5985d8ac
STIS <mahasiswa@stis.ac.id>
2023-05-23 14:55:47 +0700

    lil changes

--- app/src/main/java/com/example/datavisualization/VisualisasiActivity.java
+++ app/src/main/java/com/example/datavisualization/VisualisasiActivity.java
@@ -10,5 +10,6 @@ public class VisualisasiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualisasi);
+
     }
 }
