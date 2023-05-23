e8e63bd5bfeab04c9566ab3cf3d0ce681998447a
STIS <mahasiswa@stis.ac.id>
2023-05-23 16:06:46 +0700

    designing visualization page

--- app/src/main/java/com/example/datavisualization/BarChartActivity.java
+++ app/src/main/java/com/example/datavisualization/BarChartActivity.java
@@ -20,53 +20,17 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 import java.util.ArrayList;
 
 public class BarChartActivity extends AppCompatActivity {
-    BarDataSet barDataSet;
-    BarData barData;
-    BarChart barChart;
-
-    ArrayList<BarEntry> visitors = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bar_chart);
 
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            ArrayList<String> value = extras.getStringArrayList("table");
-            //The key argument here must match that used in the other activity
-            for(int i = 0; i < value.size(); i++){
-                String[] str = value.get(i).split(",");
-                visitors.add(new BarEntry(Integer.parseInt(str[0]), Integer.parseInt(str[1])));
-            }
-        }
-        else {
-            visitors.add(new BarEntry(2014, 420));
-            visitors.add(new BarEntry(2015, 475));
-            visitors.add(new BarEntry(2016, 508));
-            visitors.add(new BarEntry(2017, 550));
-            visitors.add(new BarEntry(2018, 650));
-            visitors.add(new BarEntry(2019, 470));
-        }
+    }
 
-        barDataSet = new BarDataSet(visitors, "Visitors");
-        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        barDataSet.setValueTextColor(Color.BLACK);
-        barDataSet.setValueTextSize(16f);
-
-        barData = new BarData(barDataSet);
-
-        barChart = findViewById(R.id.barChart);
-        barChart.setFitBars(true);
-        barChart.setData(barData);
-        barChart.getDescription().setText("Bar Chart");
-        barChart.animateY(2000);
-
-        findViewById(R.id.buttonBack).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
     }
 }
--- app/src/main/java/com/example/datavisualization/VisualisasiActivity.java
+++ app/src/main/java/com/example/datavisualization/VisualisasiActivity.java
@@ -2,14 +2,25 @@ package com.example.datavisualization;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class VisualisasiActivity extends AppCompatActivity {
+    Intent barChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualisasi);
 
+        barChart = new Intent(this, BarChartActivity.class);
+
+        findViewById(R.id.visualisasi_bar_chart_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(barChart);
+            }
+        });
     }
 }
